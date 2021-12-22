07b7f9a9e718cb982ad0ae836f160b2b25b5172e
username <email>
2021-12-22 18:55:07 +0530

    react-app4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      isClass : false,
+      isFunction : false
+    }
+  }
+  
+  render(){
+    const myStyle = {
+        color : "red",
+        textAlign : "center"
+    }
+    return(
+      <div className='App'>
+        <div>
+          <h1 style={{color:"blue",textAlign:"center"}}>Styling using Class and Functional Components</h1>
+        </div>
+        <button style={{marginTop:"90px",height:"70px",width:"350px",border:"none", align: "center", backgroundColor:"blue", borderRadius:"8px",marginLeft:"300px",color:"white", fontSize:"15px"}}onClick={()=>this.setState({isClass:!this.state.isClass})}>Click to see Class Component</button>
+        <button style={{marginTop:"90px",height:"70px",width:"350px",border:"none", align:"center", backgroundColor:"blue", borderRadius:"8px",marginLeft:"300px",color:"white",fontSize:"15px"}}onClick={()=>this.setState({isFunction:!this.state.isFunction})}>Click to see Function Component</button>
 
+        {
+          this.state.isClass
+          ?
+          <div style={{marginTop:"130px",height:"200px",width:"300px",backgroundColor:"skyblue",borderRadius:"7px",color:"yellow", marginLeft:"300px",paddingTop:"20px"}}>
+            <h2 style={{color:"purple",textAlign:"center"}}>this is class component</h2>
+            <p style={{color:"yellow",textAlign:"center"}}>this is inline class</p>
+            <p style={myStyle}>this is internal css class</p>
+          </div>
+          :
+          null
+        }
+        {
+          this.state.isFunction
+          ?
+          <div style={{marginTop:"130px",height:"200px",width:"300px",backgroundColor:"skyblue",borderRadius:"7px",color:"yellow", marginLeft:"1000px",paddingTop:"20px"}}>
+            <h2 style={{color:"purple",textAlign:"center"}}>this is Functional component</h2>
+            <p style={{color:"yellow",textAlign:"center"}}>this is inline function</p>
+            <p style={myStyle}>this is internal css function</p>
+          </div>
+          :
+          null
+        }
+      </div>
+    )
+  }
+}
 export default App;
